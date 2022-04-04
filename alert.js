f71c02030b84f32e20c65da99932f343f3c34a33
inna.amirkhanian <innaamirkhanian@gmail.com>
2022-04-04 14:48:55 +0400

    Fundamentals

---
+++ alert.js
@@ -0,0 +1,14 @@
+alert("I am JavaScript");
+let name = "Ilya";
+
+let name1 = prompt("What is your name?");
+
+alert( `hello ${name1}` )
+
+let a = 1, b = 1;
+
+let c = ++a; // ?
+let d = b++; // ?
+
+alert(c);
+alert(d)
